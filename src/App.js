a63b6abf6fc672bf539972b34374f7650e9c6b38
Ajay Wagh <ajaywagh2298@gmail.com>
2023-08-16 18:08:27 +0530

    add the route in method in app js and index js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Error from "./Pages/Error/ErrorPage";
+import Home from "./Pages/Home/HomePage";
+import NavbarComponent from "./Pages/Component/Navbar/NavbarComponent";
+import PlayQuizPage from "./Pages/Quiz/playQuizPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div  >
+          <NavbarComponent/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+            <Route path="/play-quiz" element={<PlayQuizPage/>} />
+          <Route path='*' element={<Error />}/>
+        </Routes>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import database from "./ReduxController/Database";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Provider store={database}>
+            <App />
+        </Provider>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
