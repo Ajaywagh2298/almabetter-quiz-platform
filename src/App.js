5dbf19ed3659cc6065af033506cac19e6cff42c7
gayatripatil2298 <gayatrimahale094@gmail.com>
2023-08-18 18:06:19 +0530

    add the route for myquiz-page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Error from "./Pages/Error/ErrorPage";
 import Home from "./Pages/Home/HomePage";
 import NavbarComponent from "./Pages/Component/Navbar/NavbarComponent";
 import PlayQuizPage from "./Pages/Quiz/playQuizPage";
+import MyQuiz from './Pages/Quiz/myQuizPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
             <Route path="/play-quiz" element={<PlayQuizPage/>} />
+            <Route path="/my-quiz" element={<MyQuiz />} />
           <Route path='*' element={<Error />}/>
         </Routes>
       </div>
