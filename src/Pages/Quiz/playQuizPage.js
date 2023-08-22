cb713473cba5466f39b0908b423805c2ea39216c
Ajay Wagh <ajaywagh2298@gmail.com>
2023-08-23 01:15:11 +0530

    add the play quiz page

--- src/Pages/Quiz/playQuizPage.js
+++ src/Pages/Quiz/playQuizPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import { useSelector } from 'react-redux';
 import { useRef } from 'react';
 import { useDispatch } from 'react-redux';
@@ -7,9 +7,12 @@ import { useNavigate } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import {CardActionArea, TextField} from '@mui/material';
+import {Dialog} from '@mui/material';
+import {Alert,AlertTitle} from '@mui/material';
 import "../CSS/PlayQuiz.css"
 import queFound from "../Image/queFound.png"
+import CourseCard from "../Component/Other/CourseCard";
+import {ColorsArray}from "../Component/Other/Properties";
 const PlayQuizPage = () => {
 
    const quiz = useSelector((state) => state.reducer.quiz);
@@ -17,20 +20,11 @@ const PlayQuizPage = () => {
     const name = useRef();
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [showAlert, setShowAlert] = useState(true);
 
     const play = (id) => {  //this function will run when the card is clicked.
         if (name.current.value === "") {
-            //alert("Please enter a name!");
-
-            return (
-                <div id="InfoBanner">
-                    <span className="reversed reversedRight">
-                        <span>&#9888;</span>
-                    </span>
-                    <span className="reversed reversedLeft">
-                            Please enter a name!
-                    </span>
-            </div>);
+            alert("Please enter a name!");
         }
         if(name.current.value.length<5 || name.current.value>50){
             return alert("Enter a valid name between 5 and 50 characters!")
@@ -38,30 +32,30 @@ const PlayQuizPage = () => {
         else{
             dispatch(getName(name.current.value));
             dispatch(playQuiz(id));
-            navigate("/quiz");
+           navigate("/quiz-run");
         }
     }
-
+    setTimeout(() => {
+        setShowAlert(false);
+    }, 5000);
    return (
-
         <div style={{marginTop:"100px"}} >
             <div className="mainContainer">
+                <Dialog open={showAlert} sx={{padding:'10px'}}>
+                    <Alert severity="info" className={'quiz-info'} ><AlertTitle style={{color:'#3498DB',margin:'5px'}}>Info</AlertTitle> <p >Enter your name and select the quiz you want to play.</p> </Alert>
+                </Dialog>
                 <div className="heading" sx={{marginBottom:'2%'}}>
                     <h1 >PLAY QUIZ</h1>
                 </div>
                 <div className="quiz-description" style={{textAlign:"center",fontFamily:"sans-serif"}}>
-                    <h4>Enter your name and select the quiz you want to play.</h4>
                     <div className="input-name">
-                        <div className="quiz-name">
-                            <TextField
+                        <div className="quiz-name 2">
+                            <input
                                 required
                                 className="user-name"
-                                id="outlined-required"
-                                label="Enter Player Name"
-                                placeholder={"....."}
+                                placeholder="Enter Player Name"
                                 autoFocus
-                                ref={name}
-                            />
+                                ref={name} />
                         </div>
                     </div>
                     <div className="created-quiz">
@@ -70,24 +64,13 @@ const PlayQuizPage = () => {
                             <img src={queFound} className={'question-found'} alt={'Question Not Found'}/>
                             :<div style={{display:"flex", flexDirection:"row",justifyContent:"space-around",flexWrap:"wrap"}}>
                                 {quiz.filter((el) => el.isActive === true)
-                                    .map((el) => (
-                                        <Card onClick={()=>{play(el.id)}} sx={{ width: 250, marginTop:"20px", textDecoration:"none", borderRadius:"10px", boxShadow:"2px 2px 4px black" }} key={el.id}>
-                                            <CardActionArea>
-                                                <CardContent>
-                                                    <Typography gutterBottom variant="h6" component="div" sx={{textAlign:"center"}}>
-                                                        <h4>{el.title}</h4>
-                                                    </Typography>
-                                                </CardContent>
-                                            </CardActionArea>
-                                        </Card>
-
+                                    .map((el, index) => (
+                                       <CourseCard onClick={()=>{play(el.id)}} key={el.id} data={el} color={ColorsArray[index % ColorsArray.length]} />
                                     ))}
                             </div>
                         }
                     </div>
                 </div>
-
-
             </div>
         </div>
     )
