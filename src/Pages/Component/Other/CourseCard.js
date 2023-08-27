b4bba2ead3cd340acac85c44b2b2a361cb361908
Ajay Wagh <ajaywagh2298@gmail.com>
2023-08-27 23:14:27 +0530

    minor fixes

--- src/Pages/Component/Other/CourseCard.js
+++ src/Pages/Component/Other/CourseCard.js
@@ -7,7 +7,7 @@ const CourseCard = ({ data, color, onClick}) => {
         <div className="ag-courses_item">
             <div  className="ag-courses-item_link" onClick={onClick}>
                 <div className="ag-courses-item_bg" style={{ backgroundColor: color ? color : '#433334' }}></div>
-                <img src={boy} height={'100px'}  className={'ag-courses-item_avatar'}/>
+                <img src={boy} height={'100px'}  className={'ag-courses-item_avatar'} alt={'#'}/>
                 <div className="ag-courses-item_title">
                     {data.title}
                 </div>
